cc041f4ce0164d6a2869adc9410075cf310a9d46
Phil Wang <lucidrains@gmail.com>
2021-04-23 07:54:49 -0700

    simple text and audio transformers

--- clap/clap.py
+++ clap/clap.py
@@ -62,17 +62,47 @@ class FeedForward(nn.Module):
         x = to_out(x)
         return x
 
-class CLAP(nn.Module):
+class Transformer(nn.Module):
     dim: int
     depth: int
     heads: int
+    dim_head: int = 64
 
     def setup(self):
-        self.self_attns = [(Attention(dim = self.dim, heads = self.heads), FeedForward()) for _ in range(self.depth)]
+        self.layers = [(Attention(dim = self.dim, heads = self.heads, dim_head = self.dim_head), FeedForward()) for _ in range(self.depth)]
 
     @nn.compact
     def __call__(self, x):
-        for attn, ff in self.self_attns:
+        to_norm_out = nn.LayerNorm()
+
+        for attn, ff in self.layers:
             x = attn(x) + x
             x = ff(x) + x
+
+        x = to_norm_out(x)
         return x
+
+class CLAP(nn.Module):
+    text_num_tokens: int
+    text_dim: int
+    text_depth: int
+    text_heads: int
+
+    audio_dim: int
+    audio_depth: int
+    audio_heads: int
+
+    def setup(self):
+        self.audio_encoder = Transformer(dim = self.audio_dim, depth = self.audio_depth, heads = self.audio_heads)
+        self.text_encoder = Transformer(dim = self.text_dim, depth = self.text_depth, heads = self.text_heads)
+
+    @nn.compact
+    def __call__(self, text, audio):
+        num_tokens, text_dim = self.text_num_tokens, self.text_dim
+
+        to_text_tokens = nn.Embed(num_embeddings = num_tokens, features = text_dim)
+        text = to_text_tokens(text)
+
+        enc_text = self.text_encoder(text)
+        enc_audio = self.audio_encoder(audio)
+        return enc_text, enc_audio
