9a13ebc0a59b7ef70586204c108646e372b6d36f
Charles Foster <cfoster0@ccrma.stanford.edu>
2021-05-03 09:25:22 -0700

    Reformat with black.

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'clap-jax',
-  packages = find_packages(),
-  version = '0.0.1',
-  license='MIT',
-  description = 'CLAP - Contrastive Language-Audio Pretraining',
-  author = 'Charles Foster',
-  author_email = '',
-  url = 'https://github.com/cfoster0/CLAP',
-  keywords = [
-    'artificial intelligence',
-    'deep learning',
-    'contrastive learning',
-    'audio'
-  ],
-  install_requires=[
-    'einops>=0.3',
-    'flax',
-    'jax',
-    'jaxlib'
-  ],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name="clap-jax",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
+    description="CLAP - Contrastive Language-Audio Pretraining",
+    author="Charles Foster",
+    author_email="",
+    url="https://github.com/cfoster0/CLAP",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "contrastive learning",
+        "audio",
+    ],
+    install_requires=["einops>=0.3", "flax", "jax", "jaxlib"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
