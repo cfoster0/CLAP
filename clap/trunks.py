107975d081f592e7406103690d0a21301994f07d
Charles Foster <cfoster0@ccrma.stanford.edu>
2021-05-05 21:44:26 -0700

    Fixed error with trunks.py and formatted with black.

--- clap/trunks.py
+++ clap/trunks.py
@@ -22,49 +22,53 @@ import jax.numpy as jnp
 
 
 class MlpBlock(nn.Module):
-  mlp_dim: int
+    mlp_dim: int
 
-  @nn.compact
-  def __call__(self, x):
-    y = nn.Dense(self.mlp_dim)(x)
-    y = nn.gelu(y)
-    return nn.Dense(x.shape[-1])(y)
+    @nn.compact
+    def __call__(self, x):
+        y = nn.Dense(self.mlp_dim)(x)
+        y = nn.gelu(y)
+        return nn.Dense(x.shape[-1])(y)
 
 
 class MixerBlock(nn.Module):
-  """Mixer block layer."""
-  tokens_mlp_dim: int
-  channels_mlp_dim: int
+    """Mixer block layer."""
 
-  @nn.compact
-  def __call__(self, x):
-    y = nn.LayerNorm()(x)
-    y = jnp.swapaxes(y, 0, 1)
-    y = MlpBlock(self.tokens_mlp_dim, name='token_mixing')(y)
-    y = jnp.swapaxes(y, 0, 1)
-    x = x + y
-    y = nn.LayerNorm()(x)
-    return x + MlpBlock(self.channels_mlp_dim, name='channel_mixing')(y)
+    tokens_mlp_dim: int
+    channels_mlp_dim: int
+
+    @nn.compact
+    def __call__(self, x):
+        y = nn.LayerNorm()(x)
+        y = jnp.swapaxes(y, 0, 1)
+        y = MlpBlock(self.tokens_mlp_dim, name="token_mixing")(y)
+        y = jnp.swapaxes(y, 0, 1)
+        x = x + y
+        y = nn.LayerNorm()(x)
+        return x + MlpBlock(self.channels_mlp_dim, name="channel_mixing")(y)
 
 
 class MlpMixer(nn.Module):
-  """Mixer architecture."""
-  patches: Any
-  strides: Any
-  num_classes: int
-  num_blocks: int
-  hidden_dim: int
-  tokens_mlp_dim: int
-  channels_mlp_dim: int
+    """Mixer architecture."""
 
-  @nn.compact
-  def __call__(self, inputs, *):
-    x = nn.Conv(self.hidden_dim, self.patches.size,
-                strides=self.strides.size, name='stem')(inputs)
-    x = einops.rearrange(x, 'h w c -> (h w) c')
-    for _ in range(self.num_blocks):
-      x = MixerBlock(self.tokens_mlp_dim, self.channels_mlp_dim)(x)
-    x = nn.LayerNorm(name='pre_head_layer_norm')(x)
-    x = jnp.mean(x, axis=0)
-    return nn.Dense(self.num_classes, kernel_init=nn.initializers.zeros,
-                    name='head')(x)
+    patches: Any
+    strides: Any
+    num_classes: int
+    num_blocks: int
+    hidden_dim: int
+    tokens_mlp_dim: int
+    channels_mlp_dim: int
+
+    @nn.compact
+    def __call__(self, inputs):
+        x = nn.Conv(
+            self.hidden_dim, self.patches.size, strides=self.strides.size, name="stem"
+        )(inputs)
+        x = einops.rearrange(x, "h w c -> (h w) c")
+        for _ in range(self.num_blocks):
+            x = MixerBlock(self.tokens_mlp_dim, self.channels_mlp_dim)(x)
+        x = nn.LayerNorm(name="pre_head_layer_norm")(x)
+        x = jnp.mean(x, axis=0)
+        return nn.Dense(
+            self.num_classes, kernel_init=nn.initializers.zeros, name="head"
+        )(x)
