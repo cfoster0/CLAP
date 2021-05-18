5456f66ccb4c0b62a12ccd36af1c723682baf5a3
Phil Wang <lucidrains@gmail.com>
2021-05-18 12:55:03 -0700

    add preprocess script, paired text/audio dataset, and working training script off real data

--- train.py
+++ train.py
@@ -7,14 +7,10 @@ from optax import chain, clip_by_global_norm, scale_by_adam, scale, apply_update
 
 from clap.models import CLAP
 
-def data(num_samples, batch_size, text_vocab, audio_dim, rng_key):
-    for _ in range(num_samples):
-        text = random.randint(rng_key, (batch_size, 16,), 0, text_vocab)
-        audio = random.uniform(rng_key, (batch_size, 8, audio_dim))
+# data
 
-        text_mask = np.ones((batch_size, 16), dtype = bool)
-        audio_mask = np.ones((batch_size, 8), dtype = bool)
-        yield text, audio, text_mask, audio_mask
+from torch.utils.data import DataLoader
+from clap.datasets import pair_text_spectrogram_dataset_collate_fn, PairTextSpectrogramDataset
 
 @click.command()
 @optgroup.group('Model settings')
@@ -26,14 +22,16 @@ def data(num_samples, batch_size, text_vocab, audio_dim, rng_key):
 @optgroup.option('--audio_depth', default = 1, type = int)
 @optgroup.option('--audio_heads', default = 8, type = int)
 @optgroup.group('Training settings')
+@optgroup.option('--data_folder', default = './data', type = str)
 @optgroup.option('--batch_size', default = 16, type = int)
-@optgroup.option('--epochs', default = 20, type = int)
+@optgroup.option('--epochs', default = 100, type = int)
 @optgroup.option('--learning_rate', default = 3e-4, type = float)
 @optgroup.option('--weight_decay', default = 1e-1, type = float)
 @optgroup.option('--seed', default = 0, type = int)
 @optgroup.option('--max_norm', default = 0.5, type = float)
 def train(
     *,
+    data_folder,
     batch_size,
     epochs,
     learning_rate,
@@ -51,6 +49,11 @@ def train(
     # rng
 
     rng_key = random.PRNGKey(seed)
+
+    # data
+
+    dataset = PairTextSpectrogramDataset(data_folder)
+    dl = DataLoader(dataset, batch_size = batch_size, collate_fn = pair_text_spectrogram_dataset_collate_fn, drop_last = True, shuffle = True)
 
     # model 
 
@@ -75,7 +78,10 @@ def train(
         scale(-learning_rate)
     )
 
-    text, audio, text_mask, audio_mask = next(data(1, batch_size, text_vocab, audio_dim, rng_key))
+    # init
+
+    audio, audio_mask, text, text_mask = next(iter(dl))
+
     params = model.init(rng_key, text, audio, text_mask, audio_mask)
     optim_state = optim.init(params)
 
@@ -89,7 +95,7 @@ def train(
     # train loop
 
     for _ in range(epochs):
-        for text, audio, text_mask, audio_mask in data(100, batch_size, text_vocab, audio_dim, rng_key):
+        for audio, audio_mask, text, text_mask in dl:
             loss, grads = loss_fn(params, text, audio, text_mask, audio_mask)
             updates, optim_state = optim.update(grads, optim_state, params)
             params = apply_updates(params, updates)
