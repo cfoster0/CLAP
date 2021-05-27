a8d60f39c41afda4540eda9332f168e1bb0b814a
Charles Foster <cfoster0@ccrma.stanford.edu>
2021-05-26 23:52:46 -0700

    Fixed TFRecords to store spectrograms and caption bytes, padding to constant size at runtime.

--- clap/datasets.py
+++ clap/datasets.py
@@ -9,41 +9,49 @@ from einops import rearrange
 import torch.nn.functional as F
 from torch.utils.data import Dataset, TensorDataset, ConcatDataset, IterableDataset
 
-def to_tfrecords(data, fname="data.tfrecord"):
+
+def to_tfrecords(spectrograms, captions, fname="data.tfrecord"):
     tfrecord_writer = tf.io.TFRecordWriter(fname)
-    for (spectrogram, caption) in data:
+    for (spectrogram, caption) in zip(spectrograms, captions):
         example = tf.train.Example(features=tf.train.Features(feature={
-            'spectrogram-shape': tf.train.Feature(int64_list=tf.train.Int64List(value=spectrogram.shape)),
-            'spectrogram': tf.train.Feature(float_list=tf.train.FloatList(value=spectrogram.flatten())),
-            'text': tf.train.Feature(bytes_list=tf.train.BytesList(value=[caption.encode('utf-8')])),
+            'audio': tf.train.Feature(float_list=tf.train.FloatList(value=spectrogram.flatten())),
+            'text': tf.train.Feature(int64_list=tf.train.Int64List(value=[*caption.encode('utf-8')])),
         }))
         tfrecord_writer.write(example.SerializeToString())
 
     tfrecord_writer.close()
 
 class PairTextSpectrogramTFRecords(object):
-    def __init__(self, local_or_gcs_path, batch_size, prefetch_size=0, max_audio_len=2048, max_text_len=256):
+    def __init__(self, local_or_gcs_path, batch_size, prefetch_size=0, mel_bins=80, max_audio_len=2048, max_text_len=256):
+        self.mel_bins = mel_bins
+        self.max_audio_len = max_audio_len
+        self.max_text_len = max_text_len
+
         files = tf.data.TFRecordDataset.list_files(local_or_gcs_path + '/*.tfrecord', shuffle=False)
         dataset = tf.data.TFRecordDataset(files)
         dataset = dataset.map(self.deserialize_tf_record)
-        dataset = dataset.as_numpy_iterator()
-        dataset = dataset.batch(batch_size)
+        dataset = dataset.padded_batch(batch_size, padded_shapes={
+            'audio': (max_audio_len, mel_bins),
+            'text': (max_text_len),
+        })
         dataset = dataset.prefetch(prefetch_size)
+        dataset = dataset.as_numpy_iterator()
         self.dataset = dataset
-        self.max_audio_len = max_audio_len
-        self.max_text_len = max_text_len
+
+    def files(self):
+        return self.files
     
-    @staticmethod
-    def deserialize_tf_record(record):
+    def __iter__(self):
+        return self.dataset
+
+    def deserialize_tf_record(self, record):
         tfrecord_format = {
-            'spectrogram-shape': tf.io.FixedLenSequenceFeature((), dtype=tf.int64, allow_missing=True),
-            'spectrogram': tf.io.FixedLenSequenceFeature((), dtype=tf.float32, allow_missing=True),
-            'text': tf.io.FixedLenFeature([], tf.string),
+            'spectrogram': tf.io.FixedLenSequenceFeature((self.mel_bins,), dtype=tf.float32, allow_missing=True),
+            'text': tf.io.FixedLenSequenceFeature([], dtype=tf.int64, allow_missing=True),
         }
 
         features_tensor = tf.io.parse_single_example(record, tfrecord_format)
         return features_tensor
-
     
 
 class PairTextSpectrogramDataset(Dataset):
